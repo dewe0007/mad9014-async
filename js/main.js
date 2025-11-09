a6cd87beb34c3501ffffae005be9b9ace4995ea0
Megan Dewey <dewe0007@algonquinlive.com>
2025-11-09 15:56:44 -0500

    added css formatting and started js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/* 
+Step 1: 
+init -> click listener, document.querySelector(body)
+
+Step 2: 
+click calls num.Random for rounded number (between 1-100?).
+    - If number >50, return Promise including setTimeout, then generate random hex code (use hex code method shared in slack)
+    - if number <50, return Promise including setTimeout, then return string 
+    - both numbers can use the same Promise, the Promise is just to delay the result of the click generated number
+
+Step 3: 
+f(bgCol): generate hex code
+    append CSS with new value for bg color
+        new Promise 
+            setTimeout
+            .then(generate hex code)
+            .then(append CSS)
+            .catch(sorry no color!)
+
+f(showString): pass in string
+    append HTML with new <p> element containing string
+        new Promise
+            setTimeout
+            .then(return string)
+            .then(append HTML)
+            .catch(sorry no message!)
+
+
+*/
+
+window.addEventListener('DOMContentLoaded', init);
