1016d0c24f7c397f8116f91b9105eb7e8ffda7d0
Megan Dewey <dewe0007@algonquinlive.com>
2025-11-09 18:59:04 -0500

    fixed functions to make sure the reslove calls the value required in the assignment and the delay is called accurately

--- js/main.js
+++ js/main.js
@@ -1,57 +1,44 @@
-/*
-Step 2: 
-click calls num.Random for rounded number (between 1-100?).
-    - If number >50, return Promise including setTimeout, then generate random hex code (use hex code method shared in slack)
-    - if number <50, return Promise including setTimeout, then return string 
-    - both numbers can use the same Promise, the Promise is just to delay the result of the click generated number
-
-Step 3: 
-f(bgCol): generate hex code
-    append CSS with new value for bg color
-        new Promise 
-            setTimeout
-            .then(generate hex code)
-            .then(append CSS)
-            .catch(sorry no color!)
-
-f(showString): pass in string
-    append HTML with new <p> element containing string
-        new Promise
-            setTimeout
-            .then(return string)
-            .then(append HTML)
-            .catch(sorry no message!)
-
-
-*/
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-	const btn = document.querySelector('.btn');
-	btn.addEventListener('click', getNum);
+	const body = document.querySelector('body');
+	body.addEventListener('click', getNum);
 }
 
 function getNum(ev) {
 	let num = Math.floor(Math.random() * 100);
 	console.log(num);
 	if (num % 2 === 0) {
-		return new Promise(function (resolve, reject) {
-			setTimeout(resolve, 2000);
-		}).then(function () {
-			let hex = Math.random().toString(16);
-			let extracted = hex.substring(2, 8);
-			let color = `#${extracted}`;
-			const bg = document.querySelector('body');
-			bg.style.backgroundColor = color;
+		let hexCode = new Promise(function (resolve) {
+			setTimeout(function () {
+				let hex = Math.random().toString(16);
+				let extracted = hex.substring(2, 8);
+				let color = `#${extracted}`;
+				resolve(color);
+			}, 1000 + Math.random() * 1000);
 		});
+		hexCode
+			.then(function (color) {
+				const bg = document.querySelector('body');
+				bg.style.backgroundColor = color;
+			})
+			.catch(function () {
+				console.log('Error');
+			});
 	} else {
-		return new Promise(function (resolve, reject) {
-			setTimeout(resolve, 2000);
-		}).then(function () {
-			let message = "isn't that odd!";
-			const main = document.querySelector('main');
-			main.innerHTML += message;
+		let message = new Promise(function (resolve) {
+			setTimeout(function () {
+				let string = "isn't that odd!";
+				resolve(string);
+			}, 1000 + Math.random() * 1000);
 		});
+		message
+			.then(function (string) {
+				const main = document.querySelector('main');
+				main.insertAdjacentHTML('beforeend', `<p>${string}</p>`);
+			})
+			.catch(function () {
+				console.log('Error');
+			});
 	}
 }
